9648361c494551937a690310ca2fc4f5522e2a11
John Bedette <jbedette@yahoo.com>
2017-09-18 14:51:55 -0400

    add basic flightSearch form, unconnected

--- src/components/FlightSearch.js
+++ src/components/FlightSearch.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import HotelSearch from './HotelSearch'
 
 class FlightSearch extends Component{
     constructor(){
@@ -6,16 +7,78 @@ class FlightSearch extends Component{
         this.state={
             hotelSearchDisplay:false
         }
+        this.checkHandler=this.checkHandler.bind(this)
+    }
+    displayHotel(){
+        if(this.state.hotelSearchDisplay){
+            return <HotelSearch />
+        }
+        else return
+    }
+    checkHandler(event){
+        event.preventDefault();
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;  
+        if(value){
+            this.setState({
+                [name]:value
+            })
+        }else{
+            return console.log(this.state)
+        }
     }
     render(){
         return(
             <div className='FlightSearch'>
                 <form>
-                    <input className='departureAirport' />
-                    <input className='arrivalAirport' />
-                    <input className='depDate' placeholder='departure date' />
-                    <input type='checkbox' /> 
-                    <input />
+
+                    <div>
+                        <h1>Looking for a flight?</h1>
+                    </div>
+                    
+                    <div className='searchSection'>
+                        <input className='depAirport' placeholder='departing from'/>
+                        <input className='arrAirport' placeholder='arriving at'/>
+                    </div>
+
+                    <div className='searchSection'>
+                        <input className='depDate' placeholder='departure date' />
+                        <span>return flight?
+                            <input type='checkbox' />
+                        </span>
+                        <input className='returnDate' placeholder='return Date'/>
+                    </div>
+
+                    <div className='searchSection'>
+                        <span>
+                            Seats
+                            <select className='seatsDrop'>
+                                <option value='0'>0</option>
+                                <option value='1'>1</option>
+                                <option value='2'>2</option>
+                            </select>
+                        </span>
+                        <span>
+                            Class
+                            <select className='classDrop'>
+                                <option value='economy'>Economy</option>
+                                <option value='business'>Business</option>
+                                <option value='first'>First-Class</option>
+                            </select>
+                        </span>
+                    </div>
+
+                    <div className='searchSection'>
+                        <span>Would you like to book a Hotel?
+                        <input name='hotelSearchDisplay' onChange={this.checkHandler} type='checkbox'/>
+                        </span>
+                    </div>
+                    {this.displayHotel()}
+                    <div className='searchSection'>
+                        <button>Find my Flight!</button>
+                    </div>
+
                 </form>
             </div>
         )
