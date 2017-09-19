a1ee28cbf3a89eb1200c9c517bc9773fb806c306
Nital Patel <patelnital36@gmail.com>
2017-09-19 10:31:21 -0400

    updste

---
+++ src/components/FlightDisplay.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import Flight from './Flight';
+
+class FlightDisplay extends Component {
+  constructor() {
+    super();
+    this.state = {
+      flightData: null,
+      flightDataLoaded: false,
+    };
+    // bindings
+  }
+  componentDidMount() {
+    fetch(
+      'http://developer.goibibo.com/api/search/?app_id=d09ac5c5&app_key=727ff3f0af2bda5a3443c6977cdea544&format=json&source=EWR&destination=LAX&dateofdeparture=20171220&seatingclass=E&adults=1&children=0&infants=0&counter=100',
+    ).then(
+      res => res.json(),
+    ).then((jsonRes) => {
+      this.setState({
+        flightData: jsonRes.data,
+        flightDataLoaded: true,
+      });
+    });
+  }
+  renderFlights() {
+    if (this.state.flightDataLoaded) {
+      return <Flight flightData={this.state.flightData} />;
+    }
+    return <p>loadingflights</p>;
+  }
+  render() {
+    return (
+      <div>
+        {this.renderFlights()}
+      </div>
+    );
+  }
+}
+
+export default FlightDisplay;
