1d1da0bac2f762f76f0cd295ddd4bc3ffa53dc51
John Bedette <jbedette@yahoo.com>
2017-09-19 10:07:46 -0400

    add initial login skeleton

---
+++ src/components/Login.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+class Login extends Component{
+    constructor(){
+        super();
+        this.state={
+            blank:null
+        }
+    }
+    render(){
+        return(
+            <div>
+            </div>
+        )
+    }
+}
+
+export default Login;
