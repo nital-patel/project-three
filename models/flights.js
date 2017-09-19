de813ab7de77e38ffecb27208c77f407e662a483
amisha patel <ami.patel2410@gmail.com>
2017-09-19 13:01:00 -0400

    add model controller

---
+++ models/flights.js
@@ -0,0 +1,25 @@
+const db = require('../db/config');
+
+const Flight = {};
+
+Flight.update = (flight, id) => {
+
+  return db.none(`
+
+    UPDATE flight SET
+    flightno = $1,
+    arrtime = $2,
+    origin = $3,
+    depa_time = $4,
+    duration = $5,
+    destination = $6,
+    airline = $7,
+    totalfare = $8,
+    seatsAvailable = $9
+    WHERE id = $10
+    `,
+    [flight.flightno, flight.arrtime, flight.origin, flight.depa_time, flight.duration, flight.destination, flight.airline, flight.totalfare, flight.seatsAvailable,id]
+    );
+};
+
+module.exports = Flight;
--- models/trip.js
+++ models/trip.js
@@ -2,14 +2,6 @@ const db = require('../db/config');
 
 const Trip = {};
 
-Trip.findById = id => {
-  return db.oneOrNone(`
-
-    SELECT * FROM Trips WHERE user_id = $1
-    JOIN users ON users.id = user_id
-    JOIN hotels ON Hotels.id= hotel_id
-    JOIN flights ON Flights.id = flight_id` , [id]);
-}
 
 
 Trip.update = (Trips, id) => {
