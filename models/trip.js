9cb32189055fcd3b71fd31308350769f494c60d2
amisha patel <ami.patel2410@gmail.com>
2017-09-19 13:16:04 -0400

    update complete

--- models/trip.js
+++ models/trip.js
@@ -7,10 +7,13 @@ const Trip = {};
 Trip.update = (Trips, id) => {
   return db.none(
     `
-      UPDATE Trips SELECT
-
+      UPDATE Trips SET
+      flight_id = $1,
+      hotel_id = $2
+      WHERE id = $3
     `,
+    [Trips.flight_id, Trips.hotel_id, id]
+  );
+};
 
-
-    )
-}
+module.exports = Trip;
