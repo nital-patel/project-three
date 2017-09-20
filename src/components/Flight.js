0f3894819de59e6e9dbfb154c5692fd4bca3b71b
Nital Patel <patelnital36@gmail.com>
2017-09-19 21:25:07 -0400

    make the code dry

--- src/components/Flight.js
+++ src/components/Flight.js
@@ -10,39 +10,41 @@ class Flight extends Component {
     }
 
     render() {
+        const flight = this.props.flightData.onwardflights[0];
+
         return (
             <div className="flight">
-              <div className="">
-                <h3>{this.props.flightData.onwardflights[0].airline}</h3>
-                <p> Flight {this.props.flightData.onwardflights[0].flightcode}</p>
-              </div>
-              <div className="depArr">
-                <p> Departing From {this.props.flightData.onwardflights[0].origin}
-                </p>
-                <p> Arriving At {this.props.flightData.onwardflights[0].destination}</p>
-              </div>
-              <div className="time">
-                <p>Leaving At {this.props.flightData.onwardflights[0].deptime}</p>
-                <p> Arriving At {this.props.flightData.onwardflights[0].arrtime}</p>
-                <p> Duration {this.props.flightData.onwardflights[0].duration}</p>
-              </div>
-              <div className="fareSeat">
-                <p> Total Fare {this.props.flightData.onwardflights[0].fare.totalfare}</p>
-                <p>Seats Available</p>
-              </div>
+                <div className="">
+                    <h3>{flight.airline}</h3>
+                    <p> Flight {flight.flightcode}</p>
+                </div>
+                <div className="depArr">
+                    <p> Departing From {flight.origin}
+                    </p>
+                    <p> Arriving At {flight.destination}</p>
+                </div>
+                <div className="time">
+                    <p>Leaving At {flight.deptime}</p>
+                    <p> Arriving At {flight.arrtime}</p>
+                    <p> Duration {flight.duration}</p>
+                </div>
+                <div className="fareSeat">
+                    <p> Total Fare {flight.fare.totalfare}</p>
+                    <p>Seats Available</p>
+                </div>
                 {/* Amisha, we need an action here for the route */}
-              <form method='POST' action=''>
-                <input type='hidden' name='airline' value={this.props.flightData.onwardflights[0].airline} />
-                <input type='hidden' name='flightno' value={this.props.flightData.onwardflights[0].flightcode} />
-                <input type='hidden' name='origin' value={this.props.flightData.onwardflights[0].origin}/>
-                <input type='hidden' name='destination' value={this.props.flightData.onwardflights[0].destination} />
-                <input type='hidden' name='depa_time' value={this.props.flightData.onwardflights[0].deptime} />
-                <input type='hidden' name='arrtime' value={this.props.flightData.onwardflights[0].arrtime} />
-                <input type='hidden' name='duration' value={this.props.flightData.onwardflights[0].duration} />
-                <input type='hidden' name='totalfare' value={this.fareConvert()} />
-                <input type='hidden' name='seats' value={this.props.flightData.onwardflights[0].seatsavailable} />
-                <button type='submit'>Add Flight</button>
-              </form>
+                <form method='POST' action=''>
+                    <input type='hidden' name='airline' value={flight.airline} />
+                    <input type='hidden' name='flightno' value={flight.flightcode} />
+                    <input type='hidden' name='origin' value={flight.origin}/>
+                    <input type='hidden' name='destination' value={flight.destination} />
+                    <input type='hidden' name='depa_time' value={flight.deptime} />
+                    <input type='hidden' name='arrtime' value={flight.arrtime} />
+                    <input type='hidden' name='duration' value={flight.duration} />
+                    <input type='hidden' name='totalfare' value={this.fareConvert()} />
+                    <input type='hidden' name='seats' value={flight.seatsavailable} />
+                    <button type='submit'>Add Flight</button>
+                </form>
             </div>
         )
     }
