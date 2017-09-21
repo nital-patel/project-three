bf61a09c975dec13d28282203b9bde42b8c33b53
John Bedette <jbedette@yahoo.com>
2017-09-21 16:35:50 -0400

    mid hotel search work, commit for pull update

---
+++ db/seeds/Hotel-seed.sql
@@ -0,0 +1,12 @@
+\c the_wave
+INSERT INTO Hotels (name,rating,city,city_id)
+VALUES 
+    ('Grammercy Park Hotel',4.4,'New York',1),
+    ('Ace Hotel',4.3,'New York',1),
+    ('Chelsea International Hostel',3.4,'New York',1),
+    ('The Standard, Downtown LA',4.1,'Los Angeles',2),
+    ('Shelter Hotels',3.9,'Los Angeles',2),
+    ('Sofitel Los Angeles at Beverly Hills',4.4,'Los Angeles',2),
+    ('Hard Rock Hotel Chicago',4.1,'Chicago',3),
+    ('The Alise Chicago',4.4,'Chicago',3),
+    ('Hotel Felix',3.8,'Chicago',3)
