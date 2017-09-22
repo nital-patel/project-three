2fe1ce8a8afc2d74265c698d917f2f6b99f9b6c5
Nital Patel <patelnital36@gmail.com>
2017-09-22 10:53:54 -0400

    Add header in login & register link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 
 import React, { Component } from 'react'
 
+import { Link } from 'react-router-dom';
+
 class Header extends Component {
     constructor() {
         super()
@@ -14,8 +16,12 @@ class Header extends Component {
             <div className='navBar'>
                 <span>
                     <p><img className="icon" href="https://www.projectedimage.com/content/products/2015-01/l/54b3ec70c6049.png"/>  <span className="title">The Wave: What's the wave?</span></p>
+                    <div><Link to='/register'>Register</Link><register /></div>
+                    <div><Link to='/login'>Login</Link><login /></div>
                 </span>
+
             </div>
+
         )
     }
 }
